a29e08712bbe00792fe3ee6365a916a13565aad2
Kozmikode0111 <146937191+Kozmikode0111@users.noreply.github.com>
2024-03-22 10:47:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Awesome Work!');
+// Function to toggle the hamburger menu
+function toggle() {
+  var menu = document.getElementById("hamburger");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+// Function to animate images
+function animateImages() {
+  var images = document.querySelectorAll(".project_img");
+  images.forEach(function(img) {
+    img.addEventListener("mouseover", function() {
+      img.style.transform = "scale(1.1)";
+    });
+    img.addEventListener("mouseout", function() {
+      img.style.transform = "scale(1)";
+    });
+  });
+}
+
+// Call the animateImages function when the page loads
+window.onload = function() {
+  animateImages();
+}
